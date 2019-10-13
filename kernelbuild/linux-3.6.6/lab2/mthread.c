39b5fedc2e2c7ef2205e8242f32d3fc1b97a989d
Nicholas Delamo <nicholasdelamo@hotmail.com>
2019-10-13 00:59:14 -0400

    Lab 2 stuff

---
+++ kernelbuild/linux-3.6.6/lab2/mthread.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+int SharedVariable = 0;
+
+void SimpleThread(int which)
+{
+	int num, val;
+	for (num = 0; num < 20; num++)
+	{
+		if (random() > RAND_MAX / 2)
+			usleep(10);
+
+		val = SharedVariable;
+		printf("*** thread %d sees value %d\n", which, val);
+		SharedVariable = val + 1;
+	}
+	val = SharedVariable;
+	printf("Thread %d sees final value %d\n", which, val);
+}
+
+int main(int argc, char* argv[])
+{
+	SimpleThread(atoi(argv[1]));
+}
---
+++ kernelbuild/linux-3.6.6/lab2/pthread.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+
+#define NUMTHREADS 4 // Number of threads.
+
+int SharedVariable = 0;
+pthread_mutex_t lock;
+pthread_barrier_t barr;
+
+void threadFunc(int threadId)
+{
+	// int threadId = atoi
+	int num, val;
+	for (num = 0; num < 20; num++)
+	{
+		// Compilation macro
+		#ifdef PTHREAD_SYNC
+			pthread_mutex_lock(&lock);
+		#endif
+		// Increment shared variable
+		val = SharedVariable;
+		printf("*** thread %d sees value %d\n", threadId, val);
+		SharedVariable = val + 1;
+
+		#ifdef PTHREAD_SYNC
+			pthread_mutex_unlock(&lock);
+		#endif
+	}
+	val = SharedVariable;
+	pthread_barrier_wait(&barr);
+	printf("Thread %d sees final value %d\n", threadId, val);
+}
+
+int main(int argc, char *argv[])
+{
+	int i, err;
+
+	// Number of threads is 4.
+	pthread_t threads[NUMTHREADS];
+	int threadIds[NUMTHREADS] = {0};
+
+	// Initialize mutex.
+	pthread_mutex_init(&lock, NULL);
+	// Initialize barrier.
+	pthread_barrier_init(&barr, NULL, NUMTHREADS);
+
+	for (i = 0; i < NUMTHREADS; i++)
+	{
+		// Giving thread ids.
+		threadIds[i] = i;
+
+		// Creating threads
+		err = pthread_create(&threads[i], NULL, threadFunc, (void*)&threadIds[i]);
+		
+		if(err)
+		{
+			printf("ERROR creating threads, return code %d\n", err);
+			exit(-1);
+		}
+	}
+
+	// Waiting for threads to finish.
+	for (i = 0; i < NUMTHREADS; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+
+	return 0;
+}
